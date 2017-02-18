00e735339f96615b0c6d4e0941ef4560f6c810cd
Jasong Zhang <845830229@qq.com>
2017-02-18 23:50:37 +0800

    测试

--- lite/core/Router.php
+++ lite/core/Router.php
@@ -1,5 +1,6 @@
 <?php
 namespace core;
+
 /**
  * 路由方法，控制url
  * Created by PhpStorm.
@@ -11,10 +12,12 @@ class Router{
     /**
      * 路由分发，接受的是$_SERVER['REQUEST_URI']
      * eg  : /test/test/id/54/name/56
+     * 表示访问Test控制器test方法，get传递id参数，值为54，name参数值为56
      * @param $str
      */
     public static function distribute($str){
-        $a = Config::getConfig('default_action');
-        dump($str);
+        $a = Config::getConfig('default_action',true);
+        dump($str,$a);
+        (new Request())->get();
     }
 }
